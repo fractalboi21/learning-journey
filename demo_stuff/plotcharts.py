c6a2584c1411678b5be0e5ead118f110cb5261f6
infinfraczposfunc21 <40062279+infinfraczposfunc21@users.noreply.github.com>
2024-01-05 09:00:40 +0530

    Update plotcharts.py

--- demo_stuff/plotcharts.py
+++ demo_stuff/plotcharts.py
@@ -20,3 +20,39 @@ plt.title('Basic Histogram')
  
 # Display the plot
 plt.show()
+
+
+
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+langs = ['C', 'C++', 'Java', 'Python', 'PHP']
+students = [23,17,35,29,12]
+ax.bar(langs,students)
+
+plt.show()
+
+# Import libraries
+
+from matplotlib import pyplot as plt
+
+import numpy as np
+ 
+ 
+# Creating dataset
+
+cars = ['AUDI', 'BMW', 'FORD',
+
+        'TESLA', 'JAGUAR', 'MERCEDES']
+ 
+
+data = [23, 17, 35, 29, 12, 41]
+ 
+# Creating plot
+
+fig = plt.figure(figsize =(10, 7))
+
+plt.pie(data, labels = cars)
+ 
+# show plot
+plt.show()
