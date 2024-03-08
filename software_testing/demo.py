794c5c6d1a42598cafac28c8ddd3aa24c4efdd68
fractalboi21 <40062279+fractalboi21@users.noreply.github.com>
2024-03-08 16:46:26 +0530

    Update demo.py

--- software_testing/demo.py
+++ software_testing/demo.py
@@ -1,1 +1,19 @@
 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+import time
+#https://sites.google.com/chromium.org/driver/
+service= Service(executable_path="chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+driver.get("https://google.com")
+input_element=driver.find_element(By.CLASS_NAME, "gLFyf")
+input_element.send_keys("selenium tutorial"+Keys.ENTER)
+
+
+time.sleep(10)
+
+
+driver.quit()
